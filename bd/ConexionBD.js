565e0129715e6729164a84e33f1293bec1fb6909
albertoojeda-creater <alberto.ojeda.ao2001@gmail.com>
2024-07-03 10:47:59 -0600

    Modificacion

--- bd/ConexionBD.js
+++ bd/ConexionBD.js
@@ -6,9 +6,9 @@ class ConectarBD{
     async conectarMysql(){
         try {
             this.conexion=await this.mysql.createConnection({
-                host:'127.0.0.1',
-                user:'root',
-                password:'',
+                host:'bss2nnnlwdtotfhs5l2w-mysql.services.clever-cloud.com',
+                user:'udepiakgkg49bhjk',
+                password:'IEkRX54TGORZynybNvJ2',
                 database:'tiendaut',
                 port:'3306'
             });
